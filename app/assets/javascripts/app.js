b47dcbc5cef50702ce52ffd2d003b7b2885de817
facingsouth <xin.d.liu@gmail.com>
2015-09-21 09:43:51 -0700

    setup angular

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,10 @@
+var underscore = angular.module('underscore', []);
+underscore.factory('_', ['$window', function($window) {
+  return $window._; // assumes underscore has already been loaded on the page
+}]);
+
+var pbApp = angular.module("pbApp", ['ui.router', 'restangular', 'underscore'])
+
+pbApp.controller("test", ['$scope', function($scope) {
+                      $scope.hw = "Dynamic now!";
+                   }])
